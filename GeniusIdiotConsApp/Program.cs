63ee216aefe246795b1318b33ab9fe917aeb2aa4
abs22226 <abs22226@gmail.com>
2024-11-18 18:52:35 +0300

    Add GeniusIdiotConsApp project

---
+++ GeniusIdiotConsApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GeniusIdiotConsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
