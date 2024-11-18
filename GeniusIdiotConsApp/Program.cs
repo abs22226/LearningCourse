3af5261c3f2b042be6e126b7759393ae143bc509
abs22226 <abs22226@gmail.com>
2024-11-18 18:59:17 +0300

    GeniusIdiotConsApp: Introduce questions and right answers

--- GeniusIdiotConsApp/Program.cs
+++ GeniusIdiotConsApp/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] questions = new string[5];
+            questions[0] = "Сколько будет два плюс два умноженное на два?";
+            questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
+            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
+            questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
+            questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
+
+            int[] rightAnswers = new int[5];
+            rightAnswers[0] = 6;
+            rightAnswers[1] = 9;
+            rightAnswers[2] = 25;
+            rightAnswers[3] = 60;
+            rightAnswers[4] = 2;
         }
     }
 }
