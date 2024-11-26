822875d4c802c0084fb36476ec353513c92aad1e
abs22226 <abs22226@gmail.com>
2024-11-26 19:15:36 +0300

    GeniusIdiotConsApp: Fix getting results from users storage

--- GeniusIdiotConsApp/UsersStorage.cs
+++ GeniusIdiotConsApp/UsersStorage.cs
@@ -12,16 +12,18 @@ namespace GeniusIdiotConsApp
         public static List<User> GetAll()
         {
             var value = FileProvider.GetValue("GeniusIdiotConsAppUsersStorage.txt");
-            var lines = value.Split("\n");
-            var users = new List<User>();            
+            var lines = value.Split('\n');
+            var users = new List<User>();
             foreach (var line in lines)
             {
-                var result = line.Split('#');
-                var name = result[0];
-                var score = result[1];
-                var diagnosis = result[2];
-
-                users.Add(new User(name, score, diagnosis));
+                if (!string.IsNullOrEmpty(line))
+                {
+                    var result = line.Split('#');
+                    var name = result[0];
+                    var score = result[1];
+                    var diagnosis = result[2].TrimEnd('\r');
+                    users.Add(new User(name, score, diagnosis));
+                }
             }
             return users;
         }
