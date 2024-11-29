b71b7471e465d692d5a279b6df4ac5e442aeba72
abs22226 <abs22226@gmail.com>
2024-11-29 12:37:09 +0300

    GeniusIdiotWinFormsApp: Rename main form

--- GeniusIdiotWinFormsApp/MainForm.Designer.cs
+++ GeniusIdiotWinFormsApp/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GeniusIdiotWinFormsApp
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // mainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "mainForm";
+            Text = "Гений-Идиот";
+            ResumeLayout(false);
         }
 
         #endregion
