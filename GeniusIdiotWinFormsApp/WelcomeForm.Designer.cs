73880902bb53726f96d8c9ae4646f9ca4f38c070
abs22226 <abs22226@gmail.com>
2024-12-03 23:02:51 +0300

    GeniusIdiotWinFormsApp: Fix user name assigning on welcome form closing

--- GeniusIdiotWinFormsApp/WelcomeForm.Designer.cs
+++ GeniusIdiotWinFormsApp/WelcomeForm.Designer.cs
@@ -28,48 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
             inputNameLabel = new Label();
             UserNameTextBox = new TextBox();
             StartButton = new Button();
             CommentTextLabel = new Label();
             SuspendLayout();
             // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(73, 30);
-            label1.Name = "label1";
-            label1.Size = new Size(152, 21);
-            label1.TabIndex = 0;
-            label1.Text = "Добро пожаловать!";
-            // 
             // inputNameLabel
             // 
             inputNameLabel.AutoSize = true;
             inputNameLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            inputNameLabel.Location = new Point(61, 69);
+            inputNameLabel.Location = new Point(34, 31);
             inputNameLabel.Name = "inputNameLabel";
             inputNameLabel.Size = new Size(175, 42);
             inputNameLabel.TabIndex = 1;
             inputNameLabel.Text = "Введите ваше имя:\r(символ # недопустим)";
-            inputNameLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // UserNameTextBox
             // 
             UserNameTextBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            UserNameTextBox.Location = new Point(52, 136);
+            UserNameTextBox.Location = new Point(34, 97);
             UserNameTextBox.Name = "UserNameTextBox";
-            UserNameTextBox.Size = new Size(193, 29);
+            UserNameTextBox.Size = new Size(347, 29);
             UserNameTextBox.TabIndex = 2;
             // 
             // StartButton
             // 
             StartButton.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
-            StartButton.Location = new Point(86, 242);
+            StartButton.Location = new Point(34, 219);
             StartButton.Name = "StartButton";
-            StartButton.Size = new Size(125, 48);
+            StartButton.Size = new Size(347, 48);
             StartButton.TabIndex = 3;
             StartButton.Text = "Начать";
             StartButton.UseVisualStyleBackColor = true;
@@ -79,24 +67,23 @@
             // 
             CommentTextLabel.AutoSize = true;
             CommentTextLabel.Font = new Font("Segoe UI", 12F, FontStyle.Italic, GraphicsUnit.Point);
-            CommentTextLabel.Location = new Point(70, 182);
-            CommentTextLabel.MaximumSize = new Size(193, 90);
+            CommentTextLabel.Location = new Point(34, 151);
             CommentTextLabel.Name = "CommentTextLabel";
-            CommentTextLabel.Size = new Size(159, 42);
+            CommentTextLabel.Size = new Size(164, 21);
             CommentTextLabel.TabIndex = 4;
-            CommentTextLabel.Text = "Необходимо ввести корректное имя!";
-            CommentTextLabel.TextAlign = ContentAlignment.TopCenter;
+            CommentTextLabel.Text = "Текст комментария";
             // 
             // WelcomeForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(301, 325);
+            ClientSize = new Size(414, 313);
             Controls.Add(CommentTextLabel);
             Controls.Add(StartButton);
             Controls.Add(UserNameTextBox);
             Controls.Add(inputNameLabel);
-            Controls.Add(label1);
+            MaximumSize = new Size(430, 352);
+            MinimumSize = new Size(430, 352);
             Name = "WelcomeForm";
             Text = "WelcomeForm";
             Load += WelcomeForm_Load;
@@ -105,8 +92,6 @@
         }
 
         #endregion
-
-        private Label label1;
         private Label inputNameLabel;
         public TextBox UserNameTextBox;
         private Button StartButton;
--- GeniusIdiotWinFormsApp/WelcomeForm.cs
+++ GeniusIdiotWinFormsApp/WelcomeForm.cs
@@ -10,15 +10,17 @@
         private void WelcomeForm_Load(object sender, EventArgs e)
         {
             FormClosing += WelcomeForm_FormClosing;
-
             UserNameTextBox.GotFocus += UserNameTextBox_GotFocus;
-
-            CommentTextLabel.Text = string.Empty;            
+            CommentTextLabel.Text = string.Empty;
         }
 
         private void WelcomeForm_FormClosing(object? sender, FormClosingEventArgs e)
         {
-            UserNameTextBox.Text = "Аноним";
+            var userInput = UserNameTextBox.Text;
+            if (string.IsNullOrEmpty(userInput) || userInput.Contains('#'))
+            {
+                UserNameTextBox.Text = "Аноним";
+            }
         }
 
         private void UserNameTextBox_GotFocus(object? sender, EventArgs e)
@@ -28,27 +30,16 @@
 
         private void StartButton_Click(object sender, EventArgs e)
         {
-            var name = GetUserName();
-            if (!string.IsNullOrEmpty(name))
-            {
-                UserNameTextBox.Text = name;
-                Close();
-            }
-        }
-
-        private string? GetUserName()
-        {
             var userInput = UserNameTextBox.Text;
             if (string.IsNullOrEmpty(userInput) || userInput.Contains('#'))
             {
                 CommentTextLabel.Text = "Необходимо ввести корректное имя!";
                 UserNameTextBox.Clear();
-                return null;
             }
             else
             {
-                var userName = userInput.Trim();
-                return userName;
+                UserNameTextBox.Text = userInput.Trim();
+                Close();
             }
         }
     }
