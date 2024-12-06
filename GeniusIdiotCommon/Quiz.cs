fb443c0388a1cfd3c81dc6c7419c33de57cf8a89
abs22226 <abs22226@gmail.com>
2024-12-06 23:04:19 +0300

    GeniusIdiotCommon: Extract setting score and diagnosis logic from User to Quiz class

--- GeniusIdiotCommon/Quiz.cs
+++ GeniusIdiotCommon/Quiz.cs
@@ -1,27 +1,29 @@
-﻿namespace GeniusIdiotCommon
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace GeniusIdiotCommon
 {
     public class Quiz
     {
-        public User User { get; }
-        public List<Question> Questions { get; }
+        private readonly User user;
+        private readonly List<Question> questions;
         public int Length { get; }
-        public int CorrectAnswersCount { get; set; }
+        private int correctAnswersCount;
         public Question CurrentQuestion { get; private set; }
         public int CurrentQuestionNumber { get; private set; }
-        public bool IsEnded { get { return Questions.Count == 0; } }
+        public bool IsEnded { get { return questions.Count == 0; } }
 
         public Quiz(User user)
         {
-            User = user;
-            Questions = QuestionsStorage.GetAll();
-            Length = Questions.Count;
+            this.user = user;
+            questions = QuestionsStorage.GetAll();
+            Length = questions.Count;
         }
 
         public void RandomizeCurrentQuestion()
         {
             var random = new Random();
-            var index = random.Next(0, Questions.Count);
-            CurrentQuestion = Questions[index];
+            var index = random.Next(0, questions.Count);
+            CurrentQuestion = questions[index];
             CurrentQuestionNumber++;
         }
 
@@ -29,20 +31,34 @@
         {
             if (userAnswer == CurrentQuestion.Answer)
             {
-                CorrectAnswersCount++;
+                correctAnswersCount++;
             }
-
-            Questions.Remove(CurrentQuestion);
+            questions.Remove(CurrentQuestion);
         }
 
         public void SetUserScore()
         {
-            User.SetScore(CorrectAnswersCount, Length);
+            user.Score = $"{correctAnswersCount}/{Length}";
+        }
+
+        public void ResetUserResult()
+        {
+            user.Score = string.Empty;
+            user.Diagnosis = string.Empty;
         }
 
         public void SetUserDiagnosis()
         {
-            User.SetDiagnosis(CorrectAnswersCount, Length);
+            var correctAnswersPercentage = (double)correctAnswersCount / Length * 100;
+            switch (correctAnswersPercentage)
+            {
+                case < 20: user.Diagnosis = "идиот"; break;
+                case < 40: user.Diagnosis = "кретин"; break;
+                case < 60: user.Diagnosis = "дурак"; break;
+                case < 80: user.Diagnosis = "нормальный"; break;
+                case < 100: user.Diagnosis = "талант"; break;
+                default: user.Diagnosis = "гений"; break;
+            }
         }
     }
 }
--- GeniusIdiotCommon/User.cs
+++ GeniusIdiotCommon/User.cs
@@ -2,47 +2,13 @@
 {
     public class User
     {
-        public string Name { get; }
-        public string Score { get; private set; }
-        public string Diagnosis { get; private set; }
+        public string Name { get; set; }
+        public string Score { get; set; }
+        public string Diagnosis { get; set; }
 
-        public User(string name)
+        public User()
         {
-            Name = name;
-            Score = string.Empty;
-            Diagnosis = string.Empty;
-        }
 
-        public User(string name, string score, string diagnosis)
-        {
-            Name = name;
-            Score = score;
-            Diagnosis = diagnosis;
-        }
-
-        public void SetScore(int rightAnswersCount, int startingQuestionsCount)
-        {
-            Score = $"{rightAnswersCount}/{startingQuestionsCount}";
-        }
-
-        public void ResetResult()
-        {
-            Score = string.Empty;
-            Diagnosis = string.Empty;
-        }
-
-        public void SetDiagnosis(int rightAnswersCount, int startingQuestionsCount)
-        {
-            var rightAnswersPercentage = (double)rightAnswersCount / startingQuestionsCount * 100;
-            switch (rightAnswersPercentage)
-            {
-                case < 20: Diagnosis = "идиот"; break;
-                case < 40: Diagnosis = "кретин"; break;
-                case < 60: Diagnosis = "дурак"; break;
-                case < 80: Diagnosis = "нормальный"; break;
-                case < 100: Diagnosis = "талант"; break;
-                default: Diagnosis = "гений"; break;
-            }
         }
     }
 }
