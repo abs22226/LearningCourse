e7ae53f9af239901233a162dab53a52cebed3a42
abs22226 <abs22226@gmail.com>
2025-01-06 14:37:14 +0300

    SaluteWinFormsApp: Add multiple balls creation and coloring

--- BallGamesWinFormsApp/SaluteWinFormsApp/MainForm.cs
+++ BallGamesWinFormsApp/SaluteWinFormsApp/MainForm.cs
@@ -2,18 +2,26 @@ namespace SaluteWinFormsApp
 {
     public partial class MainForm : Form
     {
+        private static Random random;
+
         public MainForm()
         {
             InitializeComponent();
+            random = new Random();
         }
 
         private void MainForm_MouseDown(object sender, MouseEventArgs e)
         {
-
-
-
-            var salute = new SaluteBall(this, Brushes.DodgerBlue, e.X, e.Y);
-            salute.Start();
+            var count = random.Next(1, 11);
+            for (int i = 0; i < count; i++)
+            {
+                var r = random.Next(0, 256);
+                var g = random.Next(0, 256);
+                var b = random.Next(0, 256);
+                var randomColorBrush = new SolidBrush(Color.FromArgb(r, g, b));
+                var salute = new SaluteBall(this, randomColorBrush, e.X, e.Y);
+                salute.Start();
+            }
         }
     }
 }
